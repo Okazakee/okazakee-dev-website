d6c74f62fb41c51b57baee3a6bedd46d2b0e6cf1
okazakee <cristiandcbusiness@gmail.com>
2022-10-12 19:42:50 +0200

    fixed labels selection behaviour

--- src/components/Navbar/MobileNavbar.jsx
+++ src/components/Navbar/MobileNavbar.jsx
@@ -7,14 +7,15 @@ export const MobileNavbar = ({SetPageName, isMobile}) => { /* //TODO ADD SWIPE G
 
     const [ShowSearchbar, SetShowSearchbar] = useState(false);
 
-    const NavbarStyle = `bg-[#8c54fb] fixed bottom-6 left-0 right-0 z-50 rounded-2xl text-sm text-center w-[90vw] h-[4rem] mx-auto pt-3`;
+    const NavbarStyle = `bg-[#8c54fb] fixed bottom-6 left-0 right-0 z-50 rounded-2xl text-sm text-center w-[90vw] h-[4rem] mx-auto pt-2.5`;
     const defaultBtnStyle = 'nav h-fit text-[#e8e8e8]';
     const selectedBtnStyle = 'nav underline underline-offset-1 h-fit text-[#090909]';
-    const iconStyle = 'h-7 w-7 relative mx-auto';
+    const iconStyle = 'h-8 w-8 relative mx-auto pointer-events-none';
 
     const SetBtnFocus = (e, page) => {
         const buttons = Array.from(document.getElementsByClassName('nav'));
         const button = e.target;
+        console.log(button);
 
         SetPageName(page);
 
@@ -28,14 +29,14 @@ export const MobileNavbar = ({SetPageName, isMobile}) => { /* //TODO ADD SWIPE G
         ?
         <Searchbox isMobile={isMobile}></Searchbox> /* //TODO ADD ANIMATION ON SHOWING, ADD CLEAR BUTTON ON RIGHT, PLACE FIXED ON TOP WHEN FOCUS */
         :
-        <div className={NavbarStyle}> {/* //TODO FIX TEXT UNDERLINE BEHAVIOUR, FIND A WAY TO TRAT BUTTON AS UNIQUE PIECE, HIDE WHEN DOWNSCROLLING */}
-            <div className='flex justify-between mx-5'>
+        <div className={NavbarStyle}> {/* //TODO HIDE nav WHEN DOWNSCROLLING */}
+            <div className='flex justify-between mx-4'>
                 <motion.button
                 whileTap={{ scale: 0.6 }}
                 onClick={(e) => SetBtnFocus(e, 'bio')}
                 className={selectedBtnStyle}>
                     <div className={iconStyle}>
-                        <Image src='/images/bio.png' alt='bioBtn' layout='fill' objectFit='scale-down' priority='true' quality={100} />
+                        <Image className='pointer-events-none' src='/images/bio.png' alt='bioBtn' layout='fill' objectFit='scale-down' priority='false' quality={100} />
                     </div>
                     Bio
                 </motion.button>
@@ -44,7 +45,7 @@ export const MobileNavbar = ({SetPageName, isMobile}) => { /* //TODO ADD SWIPE G
                 onClick={(e) => SetBtnFocus(e, 'portfolio')}
                 className={defaultBtnStyle}>
                     <div className={iconStyle}>
-                        <Image src='/images/portfolio.png' alt='bioBtn' layout='fill' objectFit='scale-down' priority='true' quality={100} />
+                        <Image className='pointer-events-none' src='/images/portfolio.png' alt='bioBtn' layout='fill' objectFit='scale-down' priority='true' quality={100} />
                     </div>
                     Portfolio
                 </motion.button>
@@ -53,16 +54,16 @@ export const MobileNavbar = ({SetPageName, isMobile}) => { /* //TODO ADD SWIPE G
                 onClick={(e) => SetBtnFocus(e, 'blog')}
                 className={defaultBtnStyle}>
                     <div className={iconStyle}>
-                        <Image src='/images/blog.png' alt='bioBtn' layout='fill' objectFit='scale-down' priority='true' quality={100} />
+                        <Image className='pointer-events-none' src='/images/blog.png' alt='bioBtn' layout='fill' objectFit='scale-down' priority='true' quality={100} />
                     </div>
-                    Blog
+                  Blog
                 </motion.button>
                 <motion.button
                 whileTap={{ scale: 0.6 }}
                 onClick={() => SetShowSearchbar(!ShowSearchbar)}
                 className={defaultBtnStyle}>
                     <div className={iconStyle}>
-                        <Image src='/images/search.png' alt='bioBtn' layout='fill' objectFit='scale-down' priority='true' quality={100} />
+                        <Image className='pointer-events-none' src='/images/search.png' alt='bioBtn' layout='fill' objectFit='scale-down' priority='true' quality={100} />
                     </div>
                     Search
                 </motion.button>
@@ -71,7 +72,7 @@ export const MobileNavbar = ({SetPageName, isMobile}) => { /* //TODO ADD SWIPE G
                 onClick={(e) => SetBtnFocus(e, 'blog')}
                 className={defaultBtnStyle}>
                     <div className={iconStyle}>
-                        <Image src='/images/social.png' alt='bioBtn' layout='fill' objectFit='scale-down' priority='true' quality={100} />
+                        <Image className='pointer-events-none' src='/images/social.png' alt='bioBtn' layout='fill' objectFit='scale-down' priority='true' quality={100} />
                     </div>
                     Socials
                 </motion.button>
