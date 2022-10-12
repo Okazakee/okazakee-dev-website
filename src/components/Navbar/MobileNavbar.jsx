53f74a0e685fb7b57b957eadfb8b8582adc461a2
okazakee <cristiandcbusiness@gmail.com>
2022-10-12 18:49:49 +0200

    fixed height & label colors, added tap animation

--- src/components/Navbar/MobileNavbar.jsx
+++ src/components/Navbar/MobileNavbar.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react'
 import Image from 'next/image';
 import { Searchbox } from './Searchbox';
+import { motion } from 'framer-motion';
 
-export const MobileNavbar = ({SetPageName, isMobile}) => {
+export const MobileNavbar = ({SetPageName, isMobile}) => { /* //TODO ADD SWIPE GESTURES TO CHAGNE PAGE */
 
     const [ShowSearchbar, SetShowSearchbar] = useState(false);
 
-    const NavbarStyle = `bg-[#8c54fb] fixed bottom-6 left-0 right-0 z-50 rounded-2xl text-sm text-center w-[90vw] h-[7vh] mx-auto pt-2`;
-    const defaultBtnStyle = 'nav hover:underline hover:underline-offset-1 hover:text-black h-fit';
-    const selectedBtnStyle = 'nav underline underline-offset-1 text-black h-fit';
-    const iconStyle = 'h-6 w-6 relative mx-auto';
+    const NavbarStyle = `bg-[#8c54fb] fixed bottom-6 left-0 right-0 z-50 rounded-2xl text-sm text-center w-[90vw] h-[4rem] mx-auto pt-3`;
+    const defaultBtnStyle = 'nav h-fit text-[#e8e8e8]';
+    const selectedBtnStyle = 'nav underline underline-offset-1 h-fit text-[#090909]';
+    const iconStyle = 'h-7 w-7 relative mx-auto';
 
     const SetBtnFocus = (e, page) => {
         const buttons = Array.from(document.getElementsByClassName('nav'));
@@ -19,56 +20,61 @@ export const MobileNavbar = ({SetPageName, isMobile}) => {
 
         buttons.map(button => button.className=defaultBtnStyle);
         button.className = selectedBtnStyle;
-      }
+    }
 
     return (
         <>
         {ShowSearchbar
         ?
-        <Searchbox isMobile={isMobile}></Searchbox>
+        <Searchbox isMobile={isMobile}></Searchbox> /* //TODO ADD ANIMATION ON SHOWING, ADD CLEAR BUTTON ON RIGHT, PLACE FIXED ON TOP WHEN FOCUS */
         :
-        <div className={NavbarStyle}>
+        <div className={NavbarStyle}> {/* //TODO FIX TEXT UNDERLINE BEHAVIOUR, FIND A WAY TO TRAT BUTTON AS UNIQUE PIECE, HIDE WHEN DOWNSCROLLING */}
             <div className='flex justify-between mx-5'>
-                <button
+                <motion.button
+                whileTap={{ scale: 0.6 }}
                 onClick={(e) => SetBtnFocus(e, 'bio')}
                 className={selectedBtnStyle}>
                     <div className={iconStyle}>
                         <Image src='/images/bio.png' alt='bioBtn' layout='fill' objectFit='scale-down' priority='true' quality={100} />
                     </div>
                     Bio
-                </button>
-                <button
+                </motion.button>
+                <motion.button
+                whileTap={{ scale: 0.6 }}
                 onClick={(e) => SetBtnFocus(e, 'portfolio')}
                 className={defaultBtnStyle}>
                     <div className={iconStyle}>
                         <Image src='/images/portfolio.png' alt='bioBtn' layout='fill' objectFit='scale-down' priority='true' quality={100} />
                     </div>
                     Portfolio
-                </button>
-                <button
+                </motion.button>
+                <motion.button
+                whileTap={{ scale: 0.6 }}
                 onClick={(e) => SetBtnFocus(e, 'blog')}
                 className={defaultBtnStyle}>
                     <div className={iconStyle}>
                         <Image src='/images/blog.png' alt='bioBtn' layout='fill' objectFit='scale-down' priority='true' quality={100} />
                     </div>
                     Blog
-                </button>
-                <button
+                </motion.button>
+                <motion.button
+                whileTap={{ scale: 0.6 }}
                 onClick={() => SetShowSearchbar(!ShowSearchbar)}
                 className={defaultBtnStyle}>
                     <div className={iconStyle}>
                         <Image src='/images/search.png' alt='bioBtn' layout='fill' objectFit='scale-down' priority='true' quality={100} />
                     </div>
                     Search
-                </button>
-                <button
+                </motion.button>
+                <motion.button
+                whileTap={{ scale: 0.6 }}
                 onClick={(e) => SetBtnFocus(e, 'blog')}
                 className={defaultBtnStyle}>
                     <div className={iconStyle}>
                         <Image src='/images/social.png' alt='bioBtn' layout='fill' objectFit='scale-down' priority='true' quality={100} />
                     </div>
                     Socials
-                </button>
+                </motion.button>
             </div>
         </div>}
             </>
