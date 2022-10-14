2401ccd1f0c857dc5d7982d312f8704a217343a5
okazakee <cristiandcbusiness@gmail.com>
2022-10-14 15:49:41 +0200

    removed useless apis, test build

--- src/components/Navbar/SocialLink.jsx
+++ src/components/Navbar/SocialLink.jsx
@@ -6,7 +6,7 @@ export const SocialLink = ({social, link}) => {
   const [isHover, SetIsHover] = useState(false);
   const Icon = Icons['Fa' + social];
   const LinkStyle = `flex items-center justify-center transition-all ease-in-out duration-[400ms] ${isHover ? 'w-28 pr-1 opacity-100' : 'opacity-0  -mr-16 w-16'}`
-  const iconStyle = `m-1 w-6 h-6 ${isHover ? 'ml-2' : 'ml-2'}`; //TODO check if is fixable the margin left stuff, i don't like that space between icons
+  const iconStyle = `m-1 w-6 h-6 ml-2`; //TODO check if is fixable the margin left stuff, i don't like that space between icons
 
   return (
     <motion.a
