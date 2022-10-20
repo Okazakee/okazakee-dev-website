b4692676d8f6f4f26172e6afc565d55ffe21f033
okazakee <cristiandcbusiness@gmail.com>
2022-10-20 21:21:46 +0200

    removed unnecessary zooms

--- src/pages/Bio/index.jsx
+++ src/pages/Bio/index.jsx
@@ -17,19 +17,17 @@ export default function Bio({bio, propic}) {
       transition={{ duration: 0.2 }}
       className='flex items-center mb-[10vh] lg:mb-[20vh]'>
         <div className=''>
-          <motion.div className='h-48 w-48 relative mx-auto' whileHover={{scale: 1.1}} whileTap={{ scale: 0.9 }}>
+          <div className='h-48 w-48 relative mx-auto'>
               <Image
               className='rounded-full'
               src={propic} alt='propic' layout='fill' objectFit='cover' priority='true' quality={100} />
-          </motion.div>
+          </div>
           <div className='flex justify-center items-center my-4'>
               <h1 className='mt-2 sm:text-3xl md:text-4xl lg:text-4xl text-2xl'>Hello, I&#39;m</h1>
-              <motion.h1
-              className='text-[#8c54fb] ml-4 text-4xl md:text-6xl'
-              whileTap={{ scale: 0.9 }}
-              whileHover={{ scale: 1.1 }}>
+              <h1
+              className='text-[#8c54fb] ml-4 text-4xl md:text-6xl'>
                 OKAZAKEE
-              </motion.h1>
+              </h1>
           </div>
             <p className='mx-6 lg:mx-24 md:mx-12 sm:mx-10 max-w-6xl text-justify sm:text-xl md:text-xl lg:text-[1.50rem] text-md'>
               { bio }
