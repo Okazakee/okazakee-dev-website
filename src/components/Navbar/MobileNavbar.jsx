6bbe4a10411c27d52c9956489b58bdea422917b7
Okazakee <cristiandcbusiness@gmail.com>
2022-10-22 11:50:58 +0200

    improved styles, all tailwind in objs

--- src/components/Navbar/MobileNavbar.jsx
+++ src/components/Navbar/MobileNavbar.jsx
@@ -4,29 +4,31 @@ import Searchbox from './Searchbox';
 
 export default function MobileNavbar({SetHideSearchbox, HideSearchbox}) {
 
-    const NavbarStyle = `fixed bottom-6 left-0 right-0 z-50 rounded-2xl text-sm text-center w-[90vw] h-[4rem] mx-auto pt-2.5 backdrop-blur-md backdrop-brightness-[.3] outline outline-1 outline-[#8c54fb]`;
-    const defaultBtnStyle = 'nav h-fit text-[#b4b4b4]';
-    const selectedBtnStyle = 'nav underline underline-offset-1 h-fit text-[#e8e8e8]';
-    const iconStyle = 'h-8 w-8 relative mx-auto pointer-events-none';
+    const styles = {
+    NavbarStyle: `fixed bottom-6 left-0 right-0 z-50 rounded-2xl text-sm text-center w-[90vw] h-[4rem] mx-auto pt-2.5 backdrop-blur-md backdrop-brightness-[.3] outline outline-1 outline-[#8c54fb]`,
+    defaultBtnStyle: 'nav h-fit text-[#b4b4b4]',
+    selectedBtnStyle: 'nav underline underline-offset-1 h-fit text-[#e8e8e8]',
+    iconStyle: 'h-8 w-8 relative mx-auto pointer-events-none'
+    }
 
     const SetBtnFocus = (e) => {
         const buttons = Array.from(document.getElementsByClassName('nav'));
         const button = e.target;
 
-        buttons.map(button => button.className=defaultBtnStyle);
-        button.className = selectedBtnStyle;
+        buttons.map(button => button.className=styles.defaultBtnStyle);
+        button.className = styles.selectedBtnStyle;
     }
 
     return (
         <>
         {HideSearchbox ?
-        <div className={NavbarStyle}>
+        <div className={styles.NavbarStyle}>
             <div className='flex justify-between mx-4'>
-                <NavBtn SetHideSearchbox={SetHideSearchbox} page='/Bio' defaultBtnStyle={defaultBtnStyle} iconStyle={iconStyle} btnimg='/images/bio.png' name='Bio' btnType='selected' SetBtnFocus={SetBtnFocus}></NavBtn>
-                <NavBtn SetHideSearchbox={SetHideSearchbox} page='/Portfolio' defaultBtnStyle={defaultBtnStyle} iconStyle={iconStyle} btnimg='/images/portfolio.png' name='Portfolio' btnType='' SetBtnFocus={SetBtnFocus}></NavBtn>
-                <NavBtn SetHideSearchbox={SetHideSearchbox} page='/Blog' defaultBtnStyle={defaultBtnStyle} iconStyle={iconStyle} btnimg='/images/blog.png' name='Blog' btnType='' SetBtnFocus={SetBtnFocus}></NavBtn>
-                <NavBtn SetHideSearchbox={SetHideSearchbox} page='' defaultBtnStyle={defaultBtnStyle} iconStyle={iconStyle} btnimg='/images/search.png' name='Search' btnType='search' SetBtnFocus={SetBtnFocus}></NavBtn>
-                <NavBtn SetHideSearchbox={SetHideSearchbox} page='' defaultBtnStyle={defaultBtnStyle} iconStyle={iconStyle} btnimg='/images/social.png' name='Socials' btnType='' SetBtnFocus={SetBtnFocus}></NavBtn>
+                <NavBtn SetHideSearchbox={SetHideSearchbox} page='/Bio' defaultBtnStyle={styles.defaultBtnStyle} iconStyle={styles.iconStyle} btnimg='/images/bio.png' name='Bio' btnType='selected' SetBtnFocus={SetBtnFocus}></NavBtn>
+                <NavBtn SetHideSearchbox={SetHideSearchbox} page='/Portfolio' defaultBtnStyle={styles.defaultBtnStyle} iconStyle={styles.iconStyle} btnimg='/images/portfolio.png' name='Portfolio' btnType='' SetBtnFocus={SetBtnFocus}></NavBtn>
+                <NavBtn SetHideSearchbox={SetHideSearchbox} page='/Blog' defaultBtnStyle={styles.defaultBtnStyle} iconStyle={styles.iconStyle} btnimg='/images/blog.png' name='Blog' btnType='' SetBtnFocus={SetBtnFocus}></NavBtn>
+                <NavBtn SetHideSearchbox={SetHideSearchbox} page='' defaultBtnStyle={styles.defaultBtnStyle} iconStyle={styles.iconStyle} btnimg='/images/search.png' name='Search' btnType='search' SetBtnFocus={SetBtnFocus}></NavBtn>
+                <NavBtn SetHideSearchbox={SetHideSearchbox} page='' defaultBtnStyle={styles.defaultBtnStyle} iconStyle={styles.iconStyle} btnimg='/images/social.png' name='Socials' btnType='' SetBtnFocus={SetBtnFocus}></NavBtn>
             </div>
         </div>
         : <Searchbox isMobile={true}></Searchbox>}
--- src/components/Navbar/SocialLink.jsx
+++ src/components/Navbar/SocialLink.jsx
@@ -5,12 +5,16 @@ import * as Icons from 'react-icons/fa';
 export const SocialLink = ({social, link}) => {
   const [isHover, SetIsHover] = useState(false);
   const Icon = Icons['Fa' + social];
-  const LinkStyle = `flex items-center justify-center transition-all ease-in-out duration-[400ms] ${isHover ? 'w-28 pr-1 opacity-100' : 'opacity-0  -mr-16 w-16'}`
-  const iconStyle = `m-1 w-6 h-6`;
+
+  const styles = {
+    mainLink: 'flex items-center transition-all ease-in-out text-xl hover:bg-[#653bba] overflow-hidden rounded-lg',
+    LinkStyle: `flex items-center justify-center transition-all ease-in-out duration-[400ms] ${isHover ? 'w-28 pr-1 opacity-100' : 'opacity-0  -mr-16 w-16'}`,
+    iconStyle: `m-1 w-6 h-6`
+  }
 
   return (
     <motion.a
-      className='flex items-center transition-all ease-in-out text-xl hover:bg-[#653bba] overflow-hidden rounded-lg'
+      className={styles.mainLink}
       whileTap={{ scale: 0.8 }}
       href={'https://' + link}
       target="_blank"
@@ -18,8 +22,8 @@ export const SocialLink = ({social, link}) => {
         <motion.div className='flex items-center'
         onHoverStart={() => SetIsHover(true)}
         onHoverEnd={() => SetIsHover(false)}>
-          <Icon className={iconStyle}/>
-          <p className={LinkStyle} onHoverStart={() => SetIsHover(true)}
+          <Icon className={styles.iconStyle}/>
+          <p className={styles.LinkStyle} onHoverStart={() => SetIsHover(true)}
           >{social}</p>
         </motion.div>
     </motion.a>
