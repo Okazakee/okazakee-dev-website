3dd74ad548e2da8f479db8303ec6cdca039e5777
okazakee <cristiandcbusiness@gmail.com>
2023-03-09 13:54:53 +0100

    auth api optimizations

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -7,7 +7,7 @@ export const getJwtSecretKey = () => {
   if (!secret || secret.length === 0) {
     throw new Error('ENV JWT_SECRET_KEY IS NOT SET!');
   }
-  console.log('tutto ok');
+
   return secret;
 };
 
@@ -19,8 +19,7 @@ export const verifyAuth = async (token) => {
     );
     return verified.payload;
   } catch (error) {
-    console.log('token', token);
-    throw Error('Your token has expired.');
+    throw Error();
   }
 };
 
@@ -29,21 +28,25 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method !== 'POST') {
-    res.status(405).json({ error: 'Method not allowed!' });
+    res
+      .status(405)
+      .json({ error: 'Method not allowed!', verifiedToken: false });
     return;
   }
 
   const { token } = req.body;
 
   if (!token) {
-    res.status(400).json({ error: 'Token is required!' });
+    res.status(400).json({ error: 'Token is required!', verifiedToken: false });
     return;
   }
 
   try {
     const payload = await verifyAuth(token);
-    res.status(200).json({ payload });
+    res.status(200).json({ payload, verifiedToken: true });
   } catch (error) {
-    res.status(401).json({ error: error.message });
+    res
+      .status(401)
+      .json({ error: 'Your token has expired!', verifiedToken: false });
   }
 }
